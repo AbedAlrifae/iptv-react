437c617e1ff134e22507f6be0bae9616db9508ed
Abed Alrifae <abedalrefai12@gmail.com>
2025-02-24 13:14:00 +0100

    update

--- src/components/IPTVPlayer.jsx
+++ src/components/IPTVPlayer.jsx
@@ -52,8 +52,8 @@ const IPTVPlayer = () => {
 
     if (Hls.isSupported()) {
       hls = new Hls();
-      const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-      hls.loadSource(proxyUrl + url);
+      const proxyUrl = "http://localhost:5000/proxy/";
+      hls.loadSource(url);
       hls.attachMedia(videoRef.current);
 
       hls.on(Hls.Events.MANIFEST_PARSED, function () {
