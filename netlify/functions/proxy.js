31d70557546c3042e7f33d71a57391d49fc454c3
Abed Alrifae <abedalrefai12@gmail.com>
2025-02-24 14:55:13 +0100

    upddate

---
+++ netlify/functions/proxy.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+  const url = 'http://m25tv.co/live/Ahmadalokla1997/964188161418/333797.m3u8'; // Het HTTP URL
+  
+  try {
+    const response = await fetch(url);
+    const data = await response.text();
+    
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/x-mpegURL'
+      },
+      body: data
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: 'Error fetching the resource'
+    };
+  }
+};
